b33433875e12ecec336de6fa406a432ed9680a43
JLJ <2669981991@qq.com>
2018-07-01 13:44:19 +0800

    需求文档

---
+++ code/generalcourse/classes/artifacts/general_course_war_exploded/background/js/index.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    console.log("dsfksaf");
+    if ($.cookie('account') != undefined &&
+        $.cookie('password') != undefined &&
+        $.cookie('account') != null &&
+        $.cookie('password') != null) {
+
+        document.getElementById('form-account').value = $.cookie('account');
+        document.getElementById('form-password').value = $.cookie('password');
+    }
+})
+
+/**
+ * 登录
+ */
+function loginClick() {
+   var account = document.getElementById('form-account').value;
+   var password = document.getElementById('form-password').value;
+
+    $.ajax({
+        url: "/login?account=" + account + "&password=" + password,
+        method: "post",
+
+        success: function (data) {
+            if (data == 1) {
+                $.cookie('account', account);
+                $.cookie('password', password);
+                location.assign("html/manage.html")
+            } else if (data == 0) {
+                swal({
+                    title: '密码错误',
+                    text: '5秒后自动关闭。',
+                    timer: 5000
+                }).then(
+                    function () {},
+                    // handling the promise rejection
+                    function (dismiss) {
+                        if (dismiss === 'timer') {
+                            console.log('I was closed by the timer')
+                        }
+                    }
+                )
+            } else if (data == -1) {
+                swal({
+                    title: '账号不存在',
+                    text: '5秒后自动关闭。',
+                    timer: 5000
+                }).then(
+                    function () {},
+                    // handling the promise rejection
+                    function (dismiss) {
+                        if (dismiss === 'timer') {
+                            console.log('I was closed by the timer')
+                        }
+                    }
+                )
+            }
+
+        },
+        error: function (data) {
+
+            sweetAlert(
+                '哎呦……',
+                '出错了！',
+                'error'
+            )
+        }
+    })
+}
